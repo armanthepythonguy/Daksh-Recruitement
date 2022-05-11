d6f6d5177fe51743684b67d0d5140a2e8eace5b1
Arman Aurobindo <arman8895884469@.com>
2022-05-11 17:20:46 +0530

    deployment

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Your JavaScript goes here
+loader = document.getElementById("preloader");
+function loading(){
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
+}
+loading();
